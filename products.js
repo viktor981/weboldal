874cf3cae585ee92df05ee6a469a9eecc17fb959
Viktor <xviktor2004@gmail.com>
2023-09-20 19:35:28 +0200

    frissitett

--- products.js
+++ products.js
@@ -1,3 +1,29 @@
-const poducts[{
-    img:
-}]
+export  const products=[{
+    id:1,
+    img:'61ayLJnlwYL.jpg',
+    name:'Bone',
+    description:"this is a fake bone",
+    price:15.00
+},
+{
+    id:2,
+    img:'8707044_R_Z001A.jpg',
+    name:'dog lead',
+    description:"this is a dig lead",
+    price:25.00
+},
+{
+    id:3,
+    img:'images.jpg',
+    name:'dog bed',
+    description:"this is a dog bed",
+    price:35.00
+},
+{
+    id:3,
+    img:'images.jpg',
+    name:'dog bed',
+    description:"this is a dog bed",
+    price:35.00
+}
+];
--- script.js
+++ script.js
@@ -1,10 +1,91 @@
-document.addEventListener("DOMContentLoaded", function () {
+const products=[{
+    id:1,
+    img:'images/61ayLJnlwYL.jpg',
+    name:'Bone',
+    description:"this is a fake bone",
+    price:15.00
+},
+{
+    id:2,
+    img:'images/8707044_R_Z001A.jpg',
+    name:'dog lead',
+    description:"this is a dig lead",
+    price:25.00
+},
+{
+    id:3,
+    img:'images/images.jpg',
+    name:'dog bed',
+    description:"this is a dog bed",
+    price:35.00
+},
+{
+    id:3,
+    img:'images/images.jpg',
+    name:'dog bed',
+    description:"this is a dog bed",
+    price:35.00
+},
+
+    {
+id:3,
+    img:'images/images.jpg',
+    name:'dog bed',
+    description:"this is a dog bed",
+    price:35.00
+},
+{
+id:3,
+    img:'images/images.jpg',
+    name:'dog bed',
+    description:"this is a dog bed",
+    price:35.00
+},{
+id:3,
+    img:'images/images.jpg',
+    name:'dog bed',
+    description:"this is a dog bed",
+    price:35.00
+},{
+    id:3,
+        img:'images/images.jpg',
+        name:'dog bed',
+        description:"this is a dog bed",
+        price:35.00
+    },{
+        id:3,
+            img:'images/images.jpg',
+            name:'dog bed',
+            description:"this is a dog bed",
+            price:35.00
+        },{
+            id:3,
+                img:'images/images.jpg',
+                name:'dog bed',
+                description:"this is a dog bed",
+                price:35.00
+            }
+]
+
+console.log(products)
+
+    const main=document.querySelector('.main-element')
     const addToCartButtons = document.querySelectorAll(".add-to-cart");
     const cartItems = document.getElementById("cart-items");
     const cartTotal = document.getElementById("cart-total");
     const clearCartButton = document.getElementById("clear-cart");
 
-    let total = 0;
+   let total = 0;
+    products.forEach(product=>{
+        main.innerHTML+=` <div class="product">
+            <p>${product.name}</p>
+            <div class='img-container'>
+                <img src="${product.img}" alt="">
+            </div>
+            <p class="desc">${product.description}</p>
+            <p class="price">$${product.price.toFixed(2)}</p>`
+    
+    })
 
     addToCartButtons.forEach((button) => {
         button.addEventListener("click", () => {
@@ -24,4 +105,3 @@ document.addEventListener("DOMContentLoaded", function () {
         total = 0;
         cartTotal.textContent = total.toFixed(2);
     });
-});
